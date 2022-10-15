582be2af883b58fe74f0c595aa6fb0fc5e667383
FelipeOthon <felipeosantiago@gmail.com>
2022-10-15 13:24:01 -0300

    enviar main java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        double largura = 10.0;
+        double comprimento = 30.0;
+        double metroQuadrado = 200.0;
+        double area = largura * comprimento;
+        double preco = area * metroQuadrado;
+
+        Locale.setDefault(Locale.US);
+
+        System.out.printf("A largura do Terreno é R$%.2f\n", largura);
+        System.out.printf("O comprimento do Terreno é R$%.2f\n", comprimento);
+        System.out.printf("O valor do metro quadrado do Terreno é R$%.2f\n", metroQuadrado);
+        System.out.printf("A area do terreno é R$%.2f, e o preco do terreno é R$%.2f  ", area, preco);
+
+        scanner.close();
+
+
     }
 }
